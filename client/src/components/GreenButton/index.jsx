925a82daea1a7a16d0443faed41b788190065f53
chessoft <chessoft@hotmail.com>
2023-09-21 21:18:42 +0200

    page user + edit user

--- client/src/components/GreenButton/index.jsx
+++ client/src/components/GreenButton/index.jsx
@@ -1,12 +1,13 @@
 import styles from "./GreenButton.module.css";
 
-const GreenButton = ({isEdit = false, isDisabled = false, content, textUnderline = false , onClick}) => (
+const GreenButton = ({isTransaction = false, isEdit = false, isDisabled = false, content, textUnderline = false , onClick}) => (
   <button
     className={`
       ${styles.greenButton} 
       ${textUnderline ? styles.textUnderline : ""} 
       ${isDisabled ? styles.disabledButton : ""}
       ${isEdit ? styles.editButton : ""}
+      ${isTransaction ? styles.transactionButton : ""}
     `} // string interpolation
     disabled={isDisabled}
     onClick={onClick} 
